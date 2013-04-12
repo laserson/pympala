e99f6c2943db2d1d5b597702b292b082ff73f287
Uri Laserson <laserson@cloudera.com>
2013-04-12 15:11:27 -0700

    setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r') as ip:
+    return ip.read()
+
+setup(
+  name='pympala',
+  version='0.7',
+  description='Python client for Cloudera Impala',
+  long_description=readme(),
+  author='Uri Laserson',
+  author_email='laserson@cloudera.com',
+  url='https://github.com/laserson/pympala',
+  package_dir={'': 'lib'},
+  packages=find_packages('lib'),
+  keywords='cloudera impala python hadoop sql hdfs mpp',
+  install_requires=[
+    'distribute',
+    'thrift>=0.9'
+  ],
+  license='BSD'
+)
